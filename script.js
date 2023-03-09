d97311cfebaca3bd65cb5a833aaf029c86a24cf1
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-03-09 08:56:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function fibonacci(num) {
-// your code here
+  if (num === 0) {
+    return 0;
+  } else if (num === 1 || num === 2) {
+    return 1;
+  } else if (num > 50) {
+    return "Number too large, please enter a number between 0 and 50";
+  } else {
+    let a = 0;
+    let b = 1;
+    let c;
+    for (let i = 2; i <= num; i++) {
+      c = a + b;
+      a = b;
+      b = c;
+    }
+    return b;
+  }
 }
+
 
 module.exports = fibonacci;
