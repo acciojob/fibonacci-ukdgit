4514b3a1132d79e4d4070050a752c511cff5c9f7
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-09-11 11:50:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
-// your code here
+  if (num === 1) return 0;
+  else if (num === 2) return 1;
+  num -= 2;
+  var num1 = 0;
+  var num2 = 1;
+  var sum;
+  var i = 0;
+  for (i = 0; i < num; i++) {
+    sum = num1 + num2;
+    num1 = num2;
+    num2 = sum;
+  }
+  return num2;
 }
 
 module.exports = fibonacci;
